1ea936fce58c489a02647708ba677b54545fbf38
DuongThuy197 <thuyduongmina2003@gmail.com>
2024-08-15 11:22:37 +0700

    Update practice_7.sql

--- practice_7.sql
+++ practice_7.sql
@@ -32,11 +32,31 @@ from ranking
 where ranking = 3
 -- EX 4:
 with count_product as (
-  SELECT user_id, transaction_date,
-  count(product_id) over(partition by user_id, transaction_date) as purchase_count
-  FROM user_transactions
+  select user_id,
+  transaction_date,
+  count(product_id) as purchase_count
+  from user_transactions
+  group by user_id, transaction_date
   order by user_id, transaction_date desc
+), 
+ranking as(
+  select *,
+  row_number() over(partition by user_id) as rk
+  from count_product
 )
-select transaction_date, user_id, purchase_count,
-rank() over(partition by user_id order by transaction_date desc) as rk
-from count_product
+select transaction_date, user_id, purchase_count
+from ranking
+where rk = 1
+order by transaction_date
+-- EX 6:
+with ranking as (
+select category, product, sum(spend) as total_spend,
+RANK() OVER (PARTITION BY category order by sum(spend) desc) AS rank
+from product_spend
+where extract(year from transaction_date) = 2022
+group by category, product
+order by category
+)
+select category, product, total_spend from ranking where rank <= 2
+-- EX 7:
+
